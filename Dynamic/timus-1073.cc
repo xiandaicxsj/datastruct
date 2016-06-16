c661ec36d13722bfef10dda9612b2dfeb445650e
joker <joker@email>
2016-06-16 17:59:37 +0800

    timu 1073/1167
    Signed-off-by: joker <joker@email>

---
+++ Dynamic/timus-1073.cc
@@ -0,0 +1,8 @@
+/* give n */
+/* 给出一个整数，求 最小的平方数能够得到该整数 */
+
+/* squary countruy */
+int main()
+{
+
+}
--- Dynamic/timus-1167.cc
+++ Dynamic/timus-1167.cc
@@ -37,11 +37,15 @@ int main()
 /* new thought */
 /* dp[i][j]  min value of j horse put into i stable */
 /* dp[i][j] = min(dp[i-1][k], cacule(k <-> j)); */
+/* ac */
+/* note the cacule should use array to acculate too */
 #include<stdio.h>
 #define MAX 500
 #define MAX_V 65535
 int dp[MAX][MAX] = {MAX_V};
 int horse[MAX];
+int count1[MAX];
+int count0[MAX];
 
 int min(int a, int b)
 {
@@ -49,15 +53,17 @@ int min(int a, int b)
 }
 int caculate(int b, int e)
 {
-	int wc = 0;
-	int bb = b;
-	while(bb <= e)
-	{
-		if (horse[bb]==1)
-			wc ++;
-		bb ++;
-	}
-	return wc * (e - b + 1 - wc);
+	int ret = 0;
+	int c1 =0;
+	int c0 =0;
+	if (b == e)
+		return 0;
+	if (horse[b])
+		c1 = 1;
+	else
+		c0 = 1;
+	ret = (count1[e] - count1[b] + c1) * (count0[e] - count0[b] + c0);
+	return ret;
 }
 
 int main()
@@ -75,9 +81,13 @@ int main()
 	{	
 
 		hidx = 1;
+		count0[0] = 0;
+		count0[1] = 0;
 		while(hidx <= horse_num)
 		{
 			scanf("%d", &horse[hidx]);
+			count0[hidx] = count0[hidx-1] + (1 - horse[hidx]);
+			count1[hidx] = count1[hidx-1] + horse[hidx];
 			hidx ++;
 		}
 		hidx = 1;
@@ -101,6 +111,7 @@ int main()
 			else
 			{
 				for (m = 1; m < hidx; m++)
+			     		dp[sidx][hidx] = min(dp[sidx][hidx], dp[sidx-1][m] + caculate(m+1, hidx));
 			     		dp[sidx][hidx] = min(dp[sidx][hidx], dp[sidx-1][m] + caculate(m+1, hidx));
 			}
 		}
