a519d772b18466bdb593de039941c2bb0258fd56
= <=>
2016-06-29 22:47:37 +0800

    1072.c
    Signed-off-by: = <=>

--- Dynamic/timus-1072.cc
+++ Dynamic/timus-1072.cc
@@ -1,21 +1,56 @@
 /* 1072.cc */
 /* */
+#include<stdio.h>
+#define MAX_VALUE 65535
 #define MAX 500
-int dp[MAX];
-int size[MAX];
+int dp[MAX] ;
+
+int min(int a, int b)
+{
+	return a>b? b:a;
+}
 
 int main()
 {
 	int max_size;
 	int size_idx = 1;
-	int i = 0 ;
-	scanf("%d", &max_size);
-	while(size_idx <= max_size)
+	int size_2 = size_idx * size_idx;
+	int size = 1;
+	int tmp_min = MAX_VALUE;
+	int tmp_size = 0;
+	int i =0;
+	while(scanf("%d", &max_size) != EOF)
 	{
-		for(i = size - 1; i >= 1; i++)
+		size_idx = 1;
+		size_2 = size_idx *size_idx;
+		size = 1;
+		tmp_min = MAX_VALUE;
+		tmp_size = 0;
+	for (i = 0; i<=max_size; i++)
+	{
+		dp[i] = MAX_VALUE;
+	}
+	while(size <= max_size)
+	{
+		if (size < size_2)
 		{
-
+			tmp_size = 1;
+			tmp_min = dp[size];
+			while(tmp_size < size)
+			{
+				tmp_min = min(tmp_min, dp[tmp_size] + dp[size - tmp_size]);
+				tmp_size ++;
+			}
+			dp[size] = tmp_min;
 		}
-		size_idx ++;
+		else
+		{
+			size_idx ++;
+			size_2 = size_idx *size_idx;
+			dp[size] = 1;
+		}
+		size ++;
+	}
+	printf("%d\n", dp[max_size]);
 	}
 }
