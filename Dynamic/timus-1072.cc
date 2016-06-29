01a3204791686b6ff0d36c214d3723ae555aa51d
joker <joker@email>
2016-06-29 17:58:55 +0800

    timus-1073
    Signed-off-by: joker <joker@email>

---
+++ Dynamic/timus-1072.cc
@@ -0,0 +1,21 @@
+/* 1072.cc */
+/* */
+#define MAX 500
+int dp[MAX];
+int size[MAX];
+
+int main()
+{
+	int max_size;
+	int size_idx = 1;
+	int i = 0 ;
+	scanf("%d", &max_size);
+	while(size_idx <= max_size)
+	{
+		for(i = size - 1; i >= 1; i++)
+		{
+
+		}
+		size_idx ++;
+	}
+}
