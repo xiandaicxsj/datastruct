9d6e042c71a98db91cc24004bcb75162e9e3f0b4
rs <rs@jk.com>
2019-07-14 15:39:09 +0800

    h1400 AC
    Signed-off-by: rs <rs@jk.com>

--- Dynamic/h1400.cc
+++ Dynamic/h1400.cc
@@ -53,52 +53,70 @@ Sample Output
  * Thinking:
  *
  * We should know which variable is the "state" variable. For example, for k phrase, what k + 1 choose should not depend on k. 
+ * Status AC
  */
+#include<string.h>
 #include<iostream>
 using namespace std;
 
-int dp[2][10000];
+long long dp[2][(1 << 11) + 1];
 int idx_is_set(int m, int i)
 {
 	return m & (1 << i);
 }
+#define  left(s, m, idx) ((s << idx) & (~(1 << m)))
 
-int cal_method(int w, int h)
+long long cal_method(int w, int h)
 {
-	int m = w * h;
+	int m = w;
 	int i = 0;
 	int j = 0;
 	int p = 0;
+	long s = 0;
+	int t = 0;
 
+	m = w > h ? h : w; 
+	if (w > h) {
+		t = w;
+		w = h;
+		h = t;
+	}
+
+	dp[p][ (1 <<m) -1] = 1;
 	for (i = 0; i< h; i++) {	
 		for(j = 0; j< w; j++) {
 			p ^= 1;
-			memset(&dp[p], 0, (1 << m));
+			memset(dp[p], 0, sizeof(dp[p]));
 
 			for(s = 0; s < (1 << m); s++) {
 				/* no */
-				if (idx_is_set(s, m - 1))
-					dp[p][s << 1] += dp[p ^ 1][s];
+				if (idx_is_set(s, m - 1)) {
+					dp[p][left(s, m, 1)] += dp[p ^ 1][s];
+				}
 				/* up */
-				if (!i && !idx_is_set(s, m - 1))
-					dp[p][(s << 1) | 1] += dp[p ^ 1][s];
+				if (i && !idx_is_set(s, m - 1)) {
+					dp[p][left(s, m, 1) | 1] += dp[p ^ 1][s];
+				}
 				/* left */
-				if (!j && idx_is_set(s, m - 1) && !idx_is_set(s, 0))
-					dp[p][(s << 1) | 3] += dp[p ^ 1][s];
+				if (j && idx_is_set(s, m - 1) && !idx_is_set(s, 0)) {
+					dp[p][left(s, m, 1) | 3] += dp[p ^ 1][s];
+				}
+
 			}
+
 		}
 	}
-	return dp[p][1 << m -1];
+	return dp[p][(1 << m) -1];
 }
 
 int main() {
 	
 	int h, w;
-	while(scanf("%d %d\n", &h, &w) !=0) {
-		if (!h || !w)
+	while (true) {
+		cin >> h >> w;
+		if (!h && !w) 
 			break;
-
-		cout << cal_method(h, w);
+		printf("%lld\n", cal_method(w, h));
 	}
 	
 	return 0;
