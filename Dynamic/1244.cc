787a48222b3e799276df60683abcb1d29090140a
joker <joker@jk.com>
2019-10-24 17:17:43 +0800

    1244 need test
    Signed-off-by: joker <joker@jk.com>

--- Dynamic/1244.cc
+++ Dynamic/1244.cc
@@ -35,3 +35,87 @@
  *
  */
 
+
+int array[MAX];
+int dp[i][j];
+int steps[MAX];
+
+int cal_per(int b, int e, int num)
+{
+
+	int b1 = b;
+	int e1 = e + num - 1;
+	int sum = 0;
+	int res ;
+
+	if (e - b + 1 < num)
+		return 0;
+
+	for (int i = b1; i < b1 + num; i ++)
+		sum += array[b1];
+
+	ret = sum;
+
+	while(e1 <= e) {
+		sum -= array[b1];
+		b1++;
+		e1 ++;
+		sum += array[e1];
+		res = max(sum, res);
+	}
+
+	return sum;
+
+#ifdef 0
+	int sum = 0;
+	int b1 = b;
+	int e1 = b;
+	int res;
+
+	sum = array[e1];
+	if (b == e)
+		return sum;
+
+	res = sum;
+	e1++;
+
+	while(e1 < e) {
+		if (sum < 0) {
+			b1 = e1;
+			sum = array[e1]; 
+			e1 ++;
+			continue;
+		}
+		res = max(res, sum);
+		sum += array[e1];
+		e1 ++;
+	}
+
+	return res;
+#endif
+}
+
+int cal_max(int a_num, int step)
+{
+	int n = 0;
+	int s = 1;
+	int k = 0;
+
+	for(n = 0; n < a_num, n++) {
+		for (s = 0; s<= step; s++)
+			dp[n][s] = 0;
+	}
+
+	for (s = 1; s <= step; s++) {
+		for(n = 0; n < a_num, n++) {
+			for (k = 0; k < n; k++)
+				dp[n][s] = max(dp[n][s], dp[k][s-1] + cal_per(k + 1, n, steps[s]));
+		}
+	}
+}
+
+int main()
+{
+	int a_num;
+ 
+}
